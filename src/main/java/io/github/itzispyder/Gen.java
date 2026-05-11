1ea41dd6c8f295198bc9f581e281fc3aa9970658
ImproperIssues <itzispyder@gmail.com>
2026-05-11 15:43:43 -0700

    added really cool function

--- src/main/java/io/github/itzispyder/Gen.java
+++ src/main/java/io/github/itzispyder/Gen.java
@@ -10,13 +10,18 @@ import io.github.itzispyder.util.Mth;
 public class Gen {
 
     public static void generateWorld(WorldManager world, Camera camera) {
+        GraphFunction func = new GraphFunction(Vector.ZERO, -50, -10, 50, 10, 0.5, (x, z) -> {
+            return (1F / 23) * Math.pow(0.5 * z, 5) * Math.sin(0.5 * x);
+        }, 0xFFFFFFFF);
+        world.addEntity(func);
+    }
+
+    private static void genTurf(WorldManager world, Camera camera) {
         int range = 100;
         GraphFunction func = new GraphFunction(Vector.ZERO, -range, -range, range, range, 2, (x, z) -> {
             return Math.cos(0.1 * x) * Math.sin(0.1 * z);
         }, 0xFF02400c);
         world.addEntity(func);
-
-        genTrees(world, camera);
     }
 
     private static void genTrees(WorldManager world, Camera camera) {
